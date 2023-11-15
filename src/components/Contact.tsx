92bc0bd943f82c4aec83b1bbb88d3ecc1d772ddd
houssamLalouli <lalouli.houssam2017@gmail.com>
2023-11-15 19:59:18 +0100

    update in contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -20,7 +20,7 @@ const Contact = () => {
           className="h-[30%] lg:h-[50%] w-[40%] lg:w-[35%] 2xl:w-[500px] ml-4  mt-10 flex items-center justify-center  rounded-tl-md rounded-br-md bg-[linear-gradient(45deg,transparent_7%,#fff_5%,#fff_93%,transparent_10%)] "
         >
           <span className="z-10 tracking-wider text-[20px] lg:text-[30px] font-bold font-Poppins text-[#2A185B]">
-            Skills
+            Contact
           </span>
         </motion.div>
       </div>
