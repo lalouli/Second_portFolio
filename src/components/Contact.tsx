2ab25a25fe8910ee411a45fbee1ee0005aaeea6e
houssamLalouli <lalouli.houssam2017@gmail.com>
2023-11-16 14:32:17 +0100

    add animation

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,17 +1,15 @@
-'use client';
-import React, { useState } from 'react'
-import { motion } from 'framer-motion'
+"use client";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
 import { Typography } from "@material-tailwind/react";
 import { IconBrandLinkedin } from "@tabler/icons-react";
 import { IconBrandGithub } from "@tabler/icons-react";
 import { IconMail } from "@tabler/icons-react";
 import { IconDeviceLandlinePhone } from "@tabler/icons-react";
 const Contact = () => {
-
-
   return (
     <div className="w-full h-screen flex flex-col gap-10">
-          <div className="h-[200px] w-full flex ">
+      <div className="h-[200px] w-full flex ">
         <motion.div
           animate={{ x: [10, 2000, 0] }}
           initial={{ opacity: 0, y: 25 }}
@@ -24,88 +22,115 @@ const Contact = () => {
           </span>
         </motion.div>
       </div>
-       <div className='w-[80%] h-full flex justify-center items-center '>
-
+      <div className="w-[80%] h-full flex justify-center items-center ">
         <div className="flex flex-row  w-[80%] h-[80%]">
-        <div className='hidden lg:flex  justify-center items-center '>
-          <motion.img
-            className="h-[800px] w-full  object-cover object-center"
-            initial={{ opacity: 0, y: 25 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1, delay: 1.9 }}
-            src="avatr.png"
-            alt="nature image"
-          />
+          <div className="hidden lg:flex  justify-center items-center ">
+            <motion.img
+              className="h-[800px] w-full  object-cover object-center"
+              initial={{ opacity: 0, y: 25 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1, delay: 1.9 }}
+              src="avatr.png"
+              alt="nature image"
+            />
           </div>
           <>
-        <div className="mt-10 flex flex-col justify-center items-center space-y-12">
-          <motion.p
-            className="w-[400px]  text-xl flex flex-row font-skranji font-bold text-[#B3EFB2] gap-2"
-            initial={{ opacity: 0, y: 25 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1, delay: 1 }}
-          >
-            <IconBrandLinkedin />
-            LinkedIn:{" "}
-            <a
-              href="https://www.linkedin.com/in/houssam-lalouli-56a40b215/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-2xl font-teko font-bold text-[#E8F1F2]"
-            >
-              LinkedIn Profile
-            </a>
-          </motion.p>
-          <motion.p
-            className="w-[400px] text-xl flex flex-row  font-skranji font-bold text-[#B3EFB2] gap-2"
-            initial={{ opacity: 0, y: 25 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1, delay: 1 }}
-          >
-            <IconBrandGithub />
-            GitHub:{" "}
-            <a
-              href="https://github.com/lalouli"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-2xl font-teko font-bold text-[#E8F1F2]"
-            >
-              GitHub Profile
-            </a>
-          </motion.p>
-          <motion.p
-            className="w-[400px] text-xl flex flex-row font-skranji font-bold text-[#B3EFB2] gap-2"
-            initial={{ opacity: 0, y: 25 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1, delay: 1 }}
-          >
-            <IconMail />
-            Email:{" "}
-            <a className="text-2xl font-teko font-bold text-[#E8F1F2]">
-            lalouli.houssam2017@gmail.com
-            </a>
+            <div className="mt-10 flex flex-col justify-center items-center space-y-12">
+              <motion.p
+                className="w-[400px]  text-xl flex flex-row font-skranji font-bold text-[#B3EFB2] gap-2"
+                initial={{ opacity: 0, y: 25 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1, delay: 1 }}
+              >
+                
+                <IconBrandLinkedin />
+               
+                LinkedIn:{" "}
               
-          </motion.p>
-          <motion.p
-            className="w-[400px] text-xl flex flex-row font-skranji font-bold text-[#B3EFB2] gap-2"
-            initial={{ opacity: 0, y: 25 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1, delay: 1 }}
-          >
-            <IconDeviceLandlinePhone />
-            Phone Number:{" "}
-            <a className="text-2xl font-teko font-bold text-[#E8F1F2]">
-              06 17 93 75 29
-            </a>
-          </motion.p>
-        </div>
-          
+                <motion.div
+                  initial={{ x: "100%" }}
+                  animate={{ x: 0 }}
+                  transition={{ duration: 3 }}
+                  className="text-2xl font-teko font-bold text-[#E8F1F2]  "
+                >
+                  <a
+                    href="https://www.linkedin.com/in/houssam-lalouli-56a40b215/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="marquee"
+                  >
+                    LinkedIn Profile
+                  </a>
+                </motion.div>
+              </motion.p>
+              <motion.p
+                className="w-[400px] text-xl flex flex-row  font-skranji font-bold text-[#B3EFB2] gap-2"
+                initial={{ opacity: 0, y: 25 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1, delay: 1 }}
+              >
+                <IconBrandGithub />
+                GitHub:{" "}
+                <motion.div
+                  initial={{ x: "100%" }}
+                  animate={{ x: 0 }}
+                  transition={{ duration: 3 }}
+                  className="marquee"
+                >
+                <a
+                  href="https://github.com/lalouli"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-2xl font-teko font-bold text-[#E8F1F2] "
+                >
+                  GitHub Profile
+                </a>
+                </motion.div>
+              </motion.p>
+              <motion.p
+                className="w-[400px] text-xl flex flex-row font-skranji font-bold text-[#B3EFB2] gap-2"
+                initial={{ opacity: 0, y: 25 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1, delay: 1 }}
+              >
+                <IconMail />
+                Email:{" "}
+                <motion.div
+                  initial={{ x: "100%" }}
+                  animate={{ x: 0 }}
+                  transition={{ duration: 5, repeat: Infinity}}
+                  className="marquee"
+                >
+                <a className="text-2xl font-teko font-bold text-[#E8F1F2]">
+                  lalouli.houssam2017@gmail.com
+                </a>
+                </motion.div>
+              </motion.p>
+              <motion.p
+                className="w-[400px] text-xl flex flex-row font-skranji font-bold text-[#B3EFB2] gap-2"
+                initial={{ opacity: 0, y: 25 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1, delay: 1 }}
+              >
+                <IconDeviceLandlinePhone />
+                Phone Number:{" "}
+                <motion.div
+                  initial={{ x: "100%" }}
+                  animate={{ x: 0 }}
+                  transition={{ duration: 5, repeat: Infinity}}
+                  className="marquee"
+                >
+                <a className="text-2xl font-teko font-bold text-[#E8F1F2]">
+                  06 17 93 75 29
+                </a>
+                </motion.div>
+              </motion.p>
+            </div>
           </>
-       
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
